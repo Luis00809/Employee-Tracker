eec5c4ff92811ab8ca36721a90bbd3578a1961bf
Luis00809 <davidcarvajal008@gmail.com>
2023-09-11 14:16:24 -0700

    adds link to walkthrough

--- db/schema.sql
+++ db/schema.sql
@@ -23,7 +23,7 @@ CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
-    role_id INT,
+    role_id INT NOT null,
     manager_id INT,
     FOREIGN KEY (role_id) references role(id)ON DELETE CASCADE ON UPDATE CASCADE,
     PRIMARY KEY (id)
